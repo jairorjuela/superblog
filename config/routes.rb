065979f6ab991c88aea14010246d74bec2deba5c
jairorjuela <genius85862@gmail.com>
2018-08-12 09:37:03 -0500

    Correccion de errores

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/posts', to: 'posts#index'
   get '/posts/new', to: 'posts#new', as: 'new_post'
-  post '/posts/create', to: 'posts#create'
-  get '/posts/:id', to: 'posts#show'
+  post '/posts', to: 'posts#create'
+  get '/posts/:id', to: 'posts#show', as: 'post'
   get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
   patch '/posts/:id', to: 'posts#update'
   delete '/posts/:id', to: 'posts#destroy'
