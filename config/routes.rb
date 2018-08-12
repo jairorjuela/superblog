4ee459298b63aaeda9e23df642cfb7b6658c1092
jairorjuela <genius85862@gmail.com>
2018-08-12 09:40:33 -0500

    Crea resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get '/posts', to: 'posts#index'
-  get '/posts/new', to: 'posts#new', as: 'new_post'
-  post '/posts', to: 'posts#create'
-  get '/posts/:id', to: 'posts#show', as: 'post'
-  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
-  patch '/posts/:id', to: 'posts#update'
-  delete '/posts/:id', to: 'posts#destroy'
+  root 'posts#index'
+  resources :posts
 end
