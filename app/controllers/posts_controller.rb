a71ad1cc8c5f40719d570ba0afae0133513b73bf
jairorjuela <genius85862@gmail.com>
2018-08-12 09:09:53 -0500

    Crea rutas y vistas

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Product.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "El post fue publicado con exito"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to posts_path, notice: "El POST ha sido modificado con exito"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,5 @@
+module PostsHelper
+  def form_title
+    @post.new_record? ? "Publicar POST" : "Modificar POST"
+  end
+end
